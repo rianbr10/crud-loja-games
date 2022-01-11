63c5fe618136cf9898169b28ddc5fff4c68bbf49
Rian Brunelli <rian10brunelli@gmail.com>
2022-01-11 16:59:14 -0300

    Css da home feito

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="register-container">
+        <h1 className="register-title">Scrim Shop</h1>
+
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          className="register-input"
+        />
+        <input
+          type="text"
+          name="cost"
+          placeholder="Preço"
+          className="register-input"
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="Categoria"
+          className="register-input"
+        />
+
+        <button className="register-button">Cadastrar</button>
+
+      </div>
     </div>
   );
 }
