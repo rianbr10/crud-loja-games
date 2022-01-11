ba5f43aba6a15f2dc568d98d4e886089fe8c2ab3
Rian Brunelli <rian10brunelli@gmail.com>
2022-01-11 18:48:19 -0300

    Mapeando games cadastrados

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Card from './components/cards/card'
 
 function App() {
+
+  const [values, setValues] = useState();
+  const [listGames, setListGames] = useState();
+
+  const handleChangeValues = (value) => {
+    setValues(prevValue => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+  }
+
+  const handleClickButton = () => {
+    Axios.post("http://localhost:3001/register", {
+      name: values.name,
+      cost: values.cost,
+      category: values.category
+    }).then((response) => {
+      console.log(response);
+    })
+  }
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getCards").then((response) => {
+      setListGames(response.data);
+    })
+  }, [])
+
   return (
     <div className="app-container">
       <div className="register-container">
@@ -11,23 +41,34 @@ function App() {
           name="name"
           placeholder="Nome"
           className="register-input"
+          onChange={handleChangeValues}
         />
         <input
           type="text"
           name="cost"
           placeholder="Preço"
           className="register-input"
+          onChange={handleChangeValues}
         />
         <input
           type="text"
           name="category"
           placeholder="Categoria"
           className="register-input"
+          onChange={handleChangeValues}
         />
 
-        <button className="register-button">Cadastrar</button>
+        <button className="register-button" onClick={() =>
+          handleClickButton()}
+        >Cadastrar</button>
 
       </div>
+
+      {typeof listGames !== "undefined" && 
+      listGames.map((value) => {
+        return <Card />
+      })}
+
     </div>
   );
 }
