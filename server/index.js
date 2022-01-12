472931a4c48dc4d7465372ae6742d84d20d1c3be
Rian Brunelli <rian10brunelli@gmail.com>
2022-01-12 12:46:13 -0300

    Projeto finalizado

--- server/index.js
+++ server/index.js
@@ -4,30 +4,23 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 const db = mysql.createPool({
-  host: "localhost", 
-  user: "root", 
-  password: "",
+  host: "localhost",
+  user: "root",
+  password: "02301008@Rian",
   database: "crudgames",
 })
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res)=>{
+app.post("/register", (req, res) => {
   const { name } = req.body;
   const { cost } = req.body;
   const { category } = req.body;
 
   let SQL = "INSERT INTO games ( name, cost, category ) VALUES ( ?,?,? )"
-  db.query(SQL, [name, cost, category], (err, result)=>{
-    console.log(err);
-  })
-})
-
-app.get("/getCards", (req, res) =>{
-  let SQL = "SELECT * from games"
-  db.query(SQL, (err, result)=>{
-    if (err){
+  db.query(SQL, [name, cost, category], (err, result) => {
+    if(err){
       console.log(err)
     } else {
       res.send(result);
@@ -35,6 +28,46 @@ app.get("/getCards", (req, res) =>{
   })
 })
 
-app.listen(3001, ()=>{
+app.get("/getCards", (req, res) => {
+  let SQL = "SELECT * from games"
+  db.query(SQL, (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(result);
+    }
+  })
+})
+
+app.put("/edit", (req, res) => {
+  const { id } = req.body;
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let SQL = "UPDATE games SET name = ?, cost = ?, category = ? WHERE idgames = ?";
+
+  db.query(SQL, [name, cost, category, id], (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.delete("/delete/:id", (req, res)=>{
+  const { id } = req.params;
+  let SQL = "DELETE FROM games WHERE idgames = ?";
+  db.query(SQL, [id], (err, result) => {
+    if(err){
+      console.log(err)
+    } else {
+      res.send(result);
+    }
+  })
+})
+
+app.listen(3001, () => {
   console.log("Servidor rodando na porta 3001");
 })
